99220aad38e112f35f9d3a43064fbda6b173bd77
Allan Kalnay <a.kalnay@sportradar.com>
2021-11-02 21:56:21 +0100

    Implements SQRT functionality

--- src/main/java/operation/exceptions/UnrecognizedOperatorException.java
+++
@@ -1,9 +0,0 @@
-package operation.exceptions;
-
-public class UnrecognizedOperatorException extends RuntimeException {
-
-    public UnrecognizedOperatorException(String operator) {
-        super(String.format("Unrecognized operator: %s", operator));
-    }
-
-}
